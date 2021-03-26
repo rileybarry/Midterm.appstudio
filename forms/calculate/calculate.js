b229ac99ef80e07ccf6ed3a3192d98ab73d87125
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-03-26 12:55:25 -0500

    second save

--- forms/calculate/calculate.js
+++ forms/calculate/calculate.js
@@ -1,10 +1,12 @@
-
-function calcAvgSquare(x, y, z) {
-  return ((x + y + z)/3) * (x ** 2);
+/*
+function calcAvgSquare(x, y, z, a) {
+  return ((x + y + z)/3) * (a ** 2);
 }
 
 let num1 = Number(prompt(`Please enter your first number.`));
 let num2 = Number(prompt(`Please enter your second number.`));
 let num3 = Number(prompt(`Please enter your third number.`));
+let num4 = Number(prompt(`Please enter your fourth number.`));
 
-alert(`The answer is ${calcAvgSquare(num1, num2, num3)}.`);
+alert(`The answer is ${calcAvgSquare(num1, num2, num3, num4)}.`);
+*/

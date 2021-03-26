10df77de1c4cba721b691b0926b350bf4fe0eba3
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-03-26 13:33:17 -0500

    Final commit

---
+++ forms/animalEC/animalEC.js
@@ -0,0 +1,13 @@
+/*
+myAnimals = ['dog', 'cat', 'horse', 'meerkat'];
+
+for (i = 0; i < 2; i ++) {
+  newAnimal = prompt(`Please enter the name of an animal.`).toLowerCase()
+
+  myAnimals.unshift(newAnimal);
+  console.log(myAnimals)
+
+  lastAnimal = myAnimals.slice(-1);
+  console.log(`The last animal is a/an ${lastAnimal}`);
+}
+*/
---
+++ forms/calculateEC/calculateEC.js
@@ -0,0 +1,24 @@
+/*
+function calcAvgSquare(x, y, z, a) {
+  return ((x + y + z)/3) * (a ** 2);
+}
+
+function divideTwo(one) {
+  return Math.pow(one,2)
+}
+
+let ask = prompt(`Enter '1' to calculate average or '2' for squared calculation`)
+
+if (ask == 1) {
+  num1 = Number(prompt(`Please enter your first number.`));
+  num2 = Number(prompt(`Please enter your second number.`));
+  num3 = Number(prompt(`Please enter your third number.`));
+  num4 = Number(prompt(`Please enter your fourth number.`));
+
+  alert(`The answer is ${calcAvgSquare(num1, num2, num3, num4)}.`);
+} else if (ask == 2) {
+  numSquare = Number(prompt(`Which number would you like to square?`))
+  
+  alert(`The answer is ${divideTwo(numSquare)}`)
+}
+*/
